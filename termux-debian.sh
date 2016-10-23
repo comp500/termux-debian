2d1efba863e418e01fbf66c85a33b328dad689e2
comp500 <comp500@users.noreply.github.com>
2016-10-23 15:33:53 +0100

    Update termux-debian.sh

--- termux-debian.sh
+++ termux-debian.sh
@@ -4,13 +4,15 @@ apt install proot tar -y
 
 # get the docker image
 
+# old image is https://github.com/tianon/docker-brew-debian/raw/5f84ff77365de2ee50655978edad2ba5004c1321/stable/rootfs.tar.xz
+
 mkdir ~/debian
 cd ~/debian
-/data/data/com.termux/files/usr/bin/wget https://github.com/tianon/docker-brew-debian/raw/5f84ff77365de2ee50655978edad2ba5004c1321/stable/rootfs.tar.xz
+/data/data/com.termux/files/usr/bin/wget https://partner-images.canonical.com/core/yakkety/current/ubuntu-yakkety-core-cloudimg-armhf-root.tar.gz
 
 # extract the rootfs
 
-/data/data/com.termux/files/usr/bin/tar xvf rootfs.tar.xz
+/data/data/com.termux/files/usr/bin/tar xvf ubuntu-yakkety-core-cloudimg-armhf-root.tar.xz
 
 # cleanup
 
